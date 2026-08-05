1ae5bf83423f88e13f82e90400765848d75acc84
sapurushilpa-cell <sapurushilpa@gmail.com>
2026-08-05 19:31:27 +0530

    Delete DSA directory

--- DSA/week1(a).py
+++
@@ -1,7 +0,0 @@
-def launch(n):
-    print(n)
-    if n>1:
-        n-=1
-        return launch(n)
-    print("LAUNCH")
-result=launch(10)   
--- DSA/week1(d).py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    if n<=1:
-        return n
-    return fib(n-1) + fib(n-2)
-n=int (input ("Enter the value of n:"))
-print ("The fibanocci series:")
-for i in range (n):
-    print(fib(i), end=" ")
